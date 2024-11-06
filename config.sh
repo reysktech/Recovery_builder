2de4ef005bc14e421ca0157dc58e50909e29de43
reysktech <39183238+reysktech@users.noreply.github.com>
2024-11-06 10:20:13 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,10 +6,10 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="A15s" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_oppo_A15s.git" # device tree link
+export DT_BRANCH="full_oppo6765-user-10-QP1A.190711.020-bedd37e98646d3a1-release-keys" # device tree branch
+export VENDOR="oppo" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
